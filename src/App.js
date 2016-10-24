b8ed41326709dfad55646265987c05fd571b3c68
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2016-10-24 13:20:27 -0400

    Separate stores, add localStorage, fix  bugs.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
-import {extendObservable, observable, computed, action, autorun} from 'mobx';
+import {extendObservable, observable, computed, action, autorun, toJS} from 'mobx';
 import {observer} from 'mobx-react';
 
 var colors = ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd']; // colorbrewer set3
-
-let lastColorIdx = 0;
-function nextColor() {
-  return colors[(lastColorIdx++) % colors.length];
-}
-
 
 class Region {
   constructor(name, color, ranges) {
@@ -26,17 +20,12 @@ function makeEmptyRanges(n) {
   return ranges;
 }
 
-class AnnotatorState {
+class AnnotationStore {
   constructor() {
     extendObservable(this, {
       reviews: [],
-      curReview: 0,
-      get curText() {return this.reviews[this.curReview];},
-      activeRegion: 0,
       regions: [],
-      setActiveRegion: action(function(i) {
-        this.activeRegion = i;
-      }),
+      lastColorIdx: 0,
       setReviews: action(function(reviews) {
         this.reviews = reviews;
         this.regions.forEach(topic => {
@@ -44,30 +33,59 @@ class AnnotatorState {
         });
       })
     });
+  };
+
+  fromJson(json) {
+    this.reviews = json.reviews;
+    this.regions = json.regions;
+  };
+
+  toJson() {
+    return toJS(this);
+  };
+
+  nextColor() {
+    var idx = this.lastColorIdx;
+    this.lastColorIdx = idx + 1;
+    return colors[idx % colors.length];
+  };
+}
+
+var annotationsStore = new AnnotationStore();
+window.annotationsStore = annotationsStore;
+if (window.localStorage.annotations){
+  annotationsStore.fromJson(JSON.parse(window.localStorage.annotations));
+} else {
+  annotationsStore.setReviews(["never had korean chicken before but this was good in comparison to our homeland version. crispy and tender chicken that was not too greasy. we also ordered topoki, a saucy plate of some chewy doughy stuff, similar to the texture of mochi. \n\nthe lack of stars were due to the long wait (made to order), the cramped space, and the not being able to get more than one itsy container of sauce. let's get it straight - it's bbq sauce, not michael jackson's sweat.", "review 2"]);
+}
+
+class UiState {
+  constructor(annotationsStore) {
+    this.annotationsStore = annotationsStore;
+    extendObservable(this, {
+      curReview: 0,
+      get curText() {return this.annotationsStore.reviews[this.curReview];},
+      activeRegion: 0,
+      setActiveRegion: action(function(i) {
+        this.activeRegion = i;
+      })
+    });
   }
 }
 
-var state = new AnnotatorState();
-state.setReviews(["never had korean chicken before but this was good in comparison to our homeland version. crispy and tender chicken that was not too greasy. we also ordered topoki, a saucy plate of some chewy doughy stuff, similar to the texture of mochi. \n\nthe lack of stars were due to the long wait (made to order), the cramped space, and the not being able to get more than one itsy container of sauce. let's get it straight - it's bbq sauce, not michael jackson's sweat.", "review 2"]);
+var uistate = new UiState(annotationsStore);
+window.uistate = uistate;
 
 
-var entityMap = {
-  "&": "&amp;",
-  "<": "&lt;",
-  ">": "&gt;",
-  '"': '&quot;',
-  "'": '&#39;',
-  "/": '&#x2F;'
-};
+autorun(function() {
+  var json = JSON.stringify(annotationsStore.toJson());
+  console.log("annotationsStore:", json);
+  window.localStorage.annotations = json;
+});
 
-function escapeHtml(string) {
-  return String(string).replace(/[&<>"'\/]/g, function (s) {
-    return entityMap[s];
-  });
-}
 
 function formattedRanges(ranges) {
-  return ranges.map(({text, style}, i) => <span style={style}>{text}</span>);
+  return ranges.map(({text, style}, i) => <span key={i} style={style}>{text}</span>);
 }
 
 function joinStyle(a, b) { return {...a, ...b};}
@@ -122,7 +140,7 @@ const TopicInstance = observer(class TopicInstance extends Component {
 
 const SidebarTopic = observer(class SidebarTopic extends Component {
   handleClick = () => {
-    state.setActiveRegion(this.props.idx);
+    uistate.setActiveRegion(this.props.idx);
   };
 
   handleEditTitle = () => {
@@ -132,11 +150,10 @@ const SidebarTopic = observer(class SidebarTopic extends Component {
   };
 
   handleRemove = (evt) => {
-    console.log(state.activeRegion, this.props.idx)
-    if (state.activeRegion >= this.props.idx) {
-      state.setActiveRegion(state.activeRegion - 1);
+    if (uistate.activeRegion >= this.props.idx) {
+      uistate.setActiveRegion(uistate.activeRegion - 1);
     }
-    state.regions.splice(this.props.idx, 1);
+    annotationsStore.regions.splice(this.props.idx, 1);
     evt.stopPropagation();
   };
 
@@ -156,7 +173,7 @@ const SidebarTopic = observer(class SidebarTopic extends Component {
         {topic.ranges[curReview].map(([start, end], i) => <TopicInstance
           key={i}
           onRemove={() => topic.ranges.splice(i, 1)}
-          text={state.curText.slice(start, end)} />)}
+          text={uistate.curText.slice(start, end)} />)}
       </div>
       </div>);
   }
@@ -164,13 +181,14 @@ const SidebarTopic = observer(class SidebarTopic extends Component {
 
 const Sidebar = observer(class Sidebar extends Component {
   handleAddRegion = () => {
-    this.props.regions.push(new Region(prompt("Name for this topic?"), nextColor(), makeEmptyRanges(state.reviews.length)));
-    state.setActiveRegion(this.props.regions.length - 1);
+    this.props.regions.push(new Region(prompt("Name for this topic?"), annotationsStore.nextColor(), makeEmptyRanges(annotationsStore.reviews.length)));
+    uistate.setActiveRegion(this.props.regions.length - 1);
   };
 
   render() {
     let {activeRegion, regions, curReview} = this.props;
     return <div>{regions.map((topic, i) => <SidebarTopic
+      key={i}
       idx={i} isActive={i === activeRegion}
       topic={topic} curReview={curReview} />)}
       <button onClick={this.handleAddRegion}>{FA('plus')} Add Topic</button>
@@ -185,28 +203,28 @@ function FA(name) {
 const MainText = observer(class MainText extends Component {
   render() {
     return <div>{
-      state.regions.map(function({name, color, ranges}, i) {
-        var annotated = [{text: state.curText, style: {}}];
-        ranges[state.curReview].forEach(function([start, end]) {
+      annotationsStore.regions.map(function({name, color, ranges}, i) {
+        var annotated = [{text: uistate.curText, style: {}}];
+        ranges[uistate.curReview].forEach(function([start, end]) {
           annotated = addFormatting(annotated, start, end, {background: color});
         });
-        return <div className="hl-layer">{formattedRanges(annotated)}</div>
+        return <div key={i} className="hl-layer">{formattedRanges(annotated)}</div>
       })}
-      <div className="real-text">{state.curText}</div>
+      <div className="real-text">{uistate.curText}</div>
     </div>;
   }
 });
 
 
 
-class App extends Component {
+const App = observer(class App extends Component {
   onMouseUp(evt) {
     var range = window.getSelection().getRangeAt(0);
     if (range.startContainer !== range.endContainer || range.startOffset === range.endOffset) return;
-    if (state.activeRegion >= state.regions.length) {
+    if (uistate.activeRegion >= annotationsStore.regions.length) {
       alert("Create a topic first, then select some text.")
     }
-    state.regions[state.activeRegion].ranges[state.curReview].push([range.startOffset, range.endOffset]);
+    annotationsStore.regions[uistate.activeRegion].ranges[uistate.curReview].push([range.startOffset, range.endOffset]);
   }
 
   render() {
@@ -216,12 +234,13 @@ class App extends Component {
           <div className="col-md-9" id="text" onMouseUp={this.onMouseUp}><MainText />
           </div>
           <div className="col-md-3" id="sidebar">
-            <Sidebar regions={state.regions} activeRegion={state.activeRegion} curReview={state.curReview} />
+            <Sidebar regions={annotationsStore.regions} activeRegion={uistate.activeRegion} curReview={uistate.curReview} />
+            <button onClick={evt => {window.localStorage.clear(); window.location.reload();}}>Reset</button>
           </div>
         </div>
       </div>
     );
   }
-}
+});
 
 export default App;
