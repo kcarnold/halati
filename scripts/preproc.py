22943726abc46ebab33b05403ac2cd924a6e8867
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2016-11-01 13:34:07 -0400

    Fix response merging.

--- scripts/preproc.py
+++ scripts/preproc.py
@@ -13,12 +13,15 @@ by_annotator = {n[5::-5]: json.load(open(n)) for n in glob.glob('data/*.json')}
 #%%
 text_key = pd.read_csv('analysis/text_key.csv')
 #%%
-question_responses = [dict(item=q['id'], annotator=k, text_idx=text_idx, condition=text_key.condition.iloc[text_idx], response=response)
+question_responses = [dict(item=q['id'], annotator=k, text_idx=text_idx, response=response)
     for k, v in by_annotator.items()
     for q in v['questions']
     for text_idx, response in enumerate(q['responses'])
     if response is not None]
-pd.DataFrame(question_responses).to_csv('analysis')
+merged_responses = pd.merge(text_key, pd.DataFrame(question_responses),
+         left_on='seq', right_on='text_idx', how='right',
+)
+merged_responses.to_csv('analysis/question_responses_2.csv')
 #%%
 chars_per_topic = [dict(annotator=annotator, item=topic['name'], text_idx=text_idx, chars=sum(b-a for a, b in ranges))
     for annotator, v in by_annotator.items()
